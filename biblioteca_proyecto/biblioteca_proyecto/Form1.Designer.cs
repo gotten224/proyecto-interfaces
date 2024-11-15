fe3b73462bbde5bc7e382f37a10ae1043930ddb4
gotten224 <123328409+gotten224@users.noreply.github.com>
2024-11-15 08:30:08 +0100

    Update Form1.Designer.cs

--- biblioteca_proyecto/biblioteca_proyecto/Form1.Designer.cs
+++ biblioteca_proyecto/biblioteca_proyecto/Form1.Designer.cs
@@ -28,55 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-
-            checkBox1 = new CheckBox();
             SuspendLayout();
-            // 
-            // checkBox1
-            // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(284, 93);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(82, 19);
-            checkBox1.TabIndex = 0;
-            checkBox1.Text = "checkBox1";
-            checkBox1.UseVisualStyleBackColor = true;
-            button1 = new Button();
-            SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(137, 86);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-
-            Controls.Add(checkBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
-            PerformLayout();
-            Controls.Add(button1);
-            Name = "Form1";
-            Text = "Form1";
-            ResumeLayout(false);
-
         }
 
         #endregion
-
-
-        private CheckBox checkBox1;
-        private Button button1;
 
     }
 }
