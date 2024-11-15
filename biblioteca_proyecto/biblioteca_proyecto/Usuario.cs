80f3aeb010f7fbdb149fc52bdb5cdd2bdedf2d65
ajcastillo1 <ajcastillo@educa.madrid.org>
2024-11-15 08:39:28 +0100

    Create Usuario.cs

---
+++ biblioteca_proyecto/biblioteca_proyecto/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca_proyecto
+{
+    internal class Usuario
+    {
+        String nombre;
+
+
+    }
+}
