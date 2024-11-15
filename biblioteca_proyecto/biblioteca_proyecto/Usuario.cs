602120b5713e83d7f9c0f745600f2d2ff4102382
ajcastillo1 <ajcastillo@educa.madrid.org>
2024-11-15 08:55:10 +0100

    Update Usuario.cs

--- biblioteca_proyecto/biblioteca_proyecto/Usuario.cs
+++ biblioteca_proyecto/biblioteca_proyecto/Usuario.cs
@@ -9,7 +9,22 @@ namespace biblioteca_proyecto
     internal class Usuario
     {
         String nombre;
+        String departamento;
+        DateTime fechaSancion;
 
+        public Usuario(string nombre, string departamento)
+        {
+            this.nombre = nombre;
+            this.departamento = departamento;
+        }
 
+        public Usuario(string nombre, string departamento, DateTime fechaSancion) : this(nombre, departamento)
+        {
+            this.fechaSancion = fechaSancion;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Departamento { get => departamento; set => departamento = value; }
+        public DateTime FechaSancion { get => fechaSancion; set => fechaSancion = value; }
     }
 }
