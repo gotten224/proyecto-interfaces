aea5a0125ba14ee9a376f5c11f96438d434f1068
ajcastillo1 <ajcastillo@educa.madrid.org>
2024-11-15 10:05:25 +0100

    Creacion del resto de clases
    Implementaion del metodo devolverLibro

---
+++ biblioteca_proyecto/biblioteca_proyecto/Devolucion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca_proyecto
+{
+    internal class Devolucion : Transaccion
+    {
+        Libro libro;
+
+        public Devolucion(DateTime fechaTransaccion, Libro libro) : base(fechaTransaccion)
+        {
+            this.libro = libro;
+        }
+
+        internal Libro Libro { get => libro; set => libro = value; }
+    }
+}
---
+++ biblioteca_proyecto/biblioteca_proyecto/Prestamo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca_proyecto
+{
+    internal class Prestamo : Transaccion
+    {
+        Persona persona;
+        Libro libro;
+        DateTime fechaDevolucion;
+
+        public Prestamo(DateTime fechaTransaccion, Persona persona, Libro libro, DateTime fechaDevolucion) : base(fechaTransaccion)
+        {
+            this.persona = persona;
+            this.libro = libro;
+            this.fechaDevolucion = fechaDevolucion;
+        }
+
+        public DateTime FechaDevolucion { get => fechaDevolucion; set => fechaDevolucion = value; }
+        internal Persona Persona { get => persona; set => persona = value; }
+        internal Libro Libro { get => libro; set => libro = value; }
+    }
+
+}
