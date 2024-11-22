91216af5c171e7de99152e2a13ad11ec1aa09822
ManuriderU <manu.sr2004@gmail.com>
2024-11-22 09:13:02 +0100

    Cambios en el formulario Prestamo

--- biblioteca_proyecto/biblioteca_proyecto/FrmPrestamo.cs
+++ biblioteca_proyecto/biblioteca_proyecto/FrmPrestamo.cs
@@ -19,31 +19,71 @@ namespace biblioteca_proyecto
 
         private void BtnAgregar_Click(object sender, EventArgs e)
         {
-
+            if (RbPrestamo.Checked) {
+                DateTime fechaActual = DateTime.Now;
+                Prestamo p = new Prestamo(fechaActual)
+                {
+                    Persona = TbNombre.Text,
+                    Libro = Convert.ToInt32(TxtIsbn.Text)
+                };
+            } else if (RbDevolucion.Checked) {
+                
+            }
         }
         private void Rbtipo_CheckedChanged(object sender, EventArgs e)
         {
             if (RbDevolucion.Checked)
             {
-                PbTipo.ImageLocation = Application.ExecutablePath + "\\..\\..\\..\\..\\img\\devult.png"; ;
+                PbTipo.ImageLocation = Application.ExecutablePath + "\\..\\..\\..\\..\\img\\devult.png";
+                TbNombre.Visible = false;
             }
             if (RbPrestamo.Checked)
             {
                 PbTipo.ImageLocation = Application.ExecutablePath + "\\..\\..\\..\\..\\img\\prest.jpg";
+                TbNombre.Visible = true;
             }
-            
+
         }
         private void RbtipoBusc_CheckedChanged(object sender, EventArgs e)
         {
             if (RbDevBusc.Checked)
             {
-                PbTipo.ImageLocation = Application.ExecutablePath + "\\..\\..\\..\\..\\img\\devult.png"; ;
+                PbTipo.ImageLocation = Application.ExecutablePath + "\\..\\..\\..\\..\\img\\devult.png";
             }
             if (RbPrestBusc.Checked)
             {
                 PbTipo.ImageLocation = Application.ExecutablePath + "\\..\\..\\..\\..\\img\\prest.jpg";
             }
 
+        }
+
+        private void TpAlta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TbNombre_Validating(object sender, CancelEventArgs e)
+        {
+            if (TbNombre.Text == "")
+            {
+                epTransacciones.SetError(TbNombre, "El campo no puede estar vacio");
+            }
+            else
+            {
+                epTransacciones.Clear();
+            }
+        }
+
+        private void TxtIsbn_Validated(object sender, EventArgs e)
+        {
+            if (TxtIsbn.Text == "")
+            {
+                epTransacciones.SetError(TxtIsbn, "El campo no puede estar vacio");
+            }
+            else
+            {
+                epTransacciones.Clear();
+            }
         }
     }
 }
