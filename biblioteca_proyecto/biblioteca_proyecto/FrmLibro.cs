f78db49e3adc1577b62c40fa4f4919d192ff5821
ajcastillo1 <ajcastillo@educa.madrid.org>
2024-11-26 09:32:05 +0100

    finalizacion formLibro
    formulario completado

--- biblioteca_proyecto/biblioteca_proyecto/FrmLibro.cs
+++ biblioteca_proyecto/biblioteca_proyecto/FrmLibro.cs
@@ -74,59 +74,72 @@ namespace biblioteca_proyecto
 
         private void BtnAgragar_Click(object sender, EventArgs e)
         {
-
-            bool error = false;
-            foreach (Control i in tabControl1.SelectedTab.Controls)
+            try
             {
-                if (EpLibro.GetError(i) != "")
+                bool error = false;
+                foreach (Control i in tabControl1.SelectedTab.Controls)
                 {
-                    error = true;
-                }
-            }
-            foreach (Libro l in Form1.libros)
-            {
-                if (l.Id == Int32.Parse(MtbIdLibro.Text))
-                {
-                    error = true;
-                }
-            }
-
-            if (!error)
-            {
-                try
-                {
-                    if (RbSala.Checked)
+                    if (EpLibro.GetError(i) != "")
                     {
-                        string titulo = TbTitulo.Text;
-                        int id = Int32.Parse(MtbIdLibro.Text);
-                        string tipo = "sala";
-                        Libro libro = new Libro(tipo, titulo, id);
-                        Form1.libros.Add(libro);
-                        MessageBox.Show("El libro se ha agregado correctamente");
-                        TbTitulo.Clear();
-                        MtbIdLibro.Clear();
+                        error = true;
                     }
-                    else if (RbAlmacen.Checked)
+                }
+                foreach (Libro l in Form1.libros)
+                {
+                    if (l.Id == Int32.Parse(MtbIdLibro.Text))
                     {
-                        string titulo = TbTitulo.Text;
-                        int id = Int32.Parse(MtbIdLibro.Text);
-                        string tipo = "almacen";
-                        Libro libro = new Libro(tipo, titulo, id);
-                        Form1.libros.Add(libro);
-                        MessageBox.Show("El libro se ha agregado correctamente");
-                        TbTitulo.Clear();
-                        MtbIdLibro.Clear();
+                        error = true;
                     }
+                }
+
+                if (!error)
+                {
+                    try
+                    {
+                        if (RbSala.Checked)
+                        {
+                            string titulo = TbTitulo.Text;
+                            int id = Int32.Parse(MtbIdLibro.Text);
+                            string tipo = "sala";
+                            Libro libro = new Libro(tipo, titulo, id);
+                            Form1.libros.Add(libro);
+                            MessageBox.Show("El libro se ha agregado correctamente");
+                            TbTitulo.Clear();
+                            MtbIdLibro.Clear();
+                            RbAlmacen.Checked = false;
+                            RbSala.Checked = false;
+                            PbUbicacion.Image = null;
+                        }
+                        else if (RbAlmacen.Checked)
+                        {
+                            string titulo = TbTitulo.Text;
+                            int id = Int32.Parse(MtbIdLibro.Text);
+                            string tipo = "almacen";
+                            Libro libro = new Libro(tipo, titulo, id);
+                            Form1.libros.Add(libro);
+                            MessageBox.Show("El libro se ha agregado correctamente");
+                            TbTitulo.Clear();
+                            MtbIdLibro.Clear();
+                            RbAlmacen.Checked = false;
+                            RbSala.Checked = false;
+                            PbUbicacion.Image = null;
+                        }
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show("No se ha podido guardar el libro");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No se ha podido guardar el libro porque contiene algun error");
 
                 }
-                catch
-                {
-                    MessageBox.Show("No se ha podido guardar el libro");
-                }
             }
-            else
+            catch
             {
-                MessageBox.Show("No se ha podido guardar el libro porque contiene algun error");
+                MessageBox.Show("Ha ocurrido un error al agregar un usuario");
             }
         }
 
@@ -148,8 +161,9 @@ namespace biblioteca_proyecto
         private void CbUbi_SelectedIndexChanged(object sender, EventArgs e)
         {
             LvListar.Items.Clear();
-            foreach (Libro libro in Form1.libros){
-                if(CbUbi.Text == libro.Tipo)
+            foreach (Libro libro in Form1.libros)
+            {
+                if (CbUbi.Text == libro.Tipo)
                 {
                     ListViewItem item = new ListViewItem(libro.Id.ToString());
                     item.SubItems.Add(libro.Titulo);
@@ -158,11 +172,101 @@ namespace biblioteca_proyecto
                 }
 
             }
-            
+
         }
 
+        private void PbBorr_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int indice = LvListar.SelectedItems[0].Index;
+                Libro libroSeleccionado = Form1.libros[indice];
+                Form1.libros.RemoveAt(indice);
+                LvListar.Items.Clear();
+                foreach (Libro libro in Form1.libros)
+                {
+                    if (CbUbi.Text == libro.Tipo)
+                    {
+                        ListViewItem item = new ListViewItem(libro.Id.ToString());
+                        item.SubItems.Add(libro.Titulo);
+                        item.SubItems.Add(libro.Tipo);
+                        LvListar.Items.Add(item);
+                    }
+                    else
+                    {
+                        ListViewItem item = new ListViewItem(libro.Id.ToString());
+                        item.SubItems.Add(libro.Titulo);
+                        item.SubItems.Add(libro.Tipo);
+                        LvListar.Items.Add(item);
+                    }
+
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Ha ocurrido un error al intentar borrar un usuario");
+            }
+
+        }
+
+        private void BtnBusc_Click(object sender, EventArgs e)
+        {
+            String nombreLibroBusc = TxtBusc.Text;
+            foreach (Libro l in Form1.libros)
+            {
+                if (l.Titulo == nombreLibroBusc)
+                {
+                    TbtituloBusc.Text = l.Titulo;
+                    mskTbIdBusc.Text = l.Id.ToString();
+                    if (l.Tipo == "sala")
+                    {
+                        RbSalaBusc.Checked = true;
+                    }
+                    else if (l.Tipo == "almacen")
+                    {
+                        RbAlmaBusc.Checked = true;
+                    }
+                }
+            }
+        }
+
+        private void PbModif_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String tituloCambiar = TbtituloBusc.Text;
+                int idCambiar = Int32.Parse(mskTbIdBusc.Text);
+                String ubiCambiar = "";
+                if (RbSalaBusc.Checked)
+                {
+                    ubiCambiar = "sala";
+                }
+                else if (RbAlmaBusc.Checked)
+                {
+                    ubiCambiar = "almacen";
+                }
+                foreach (Libro l in Form1.libros)
+                {
+                    if (l.Id == idCambiar)
+                    {
+                        l.Titulo = tituloCambiar;
+                        l.Id = idCambiar;
+                        l.Tipo = ubiCambiar;
+                    }
+                }
+                MessageBox.Show("Se ha actualizado la informacion del libro " + tituloCambiar);
+
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Ha ocurrido un error al actualizar el libro");
+            }
+            TbtituloBusc.Clear();
+            mskTbIdBusc.Clear();
+            RbAlmaBusc.Checked = false;
+            RbSalaBusc.Checked = false;
+            PbBusc.Image = null;
 
 
-
+        }
     }
 }
